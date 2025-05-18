6e0ae2136ed8b21b40f6ecd70593bd0096a523f4
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-05-18 13:23:12 -0700

    Add solution for add_two_numbers with loop unrolling.

--- src/linked_list/medium/add_two_numbers_iteratively_loop_unroll.rs
+++ src/linked_list/medium/add_two_numbers_iteratively_loop_unroll.rs
@@ -1,7 +1,56 @@
 /*
 Unrolling the Loop:
-    We can manually unroll the iteration loop to handle multiple nodes in a single iteration. However, this may not be 
+    We can manually unroll the iteration loop to handle multiple nodes in a single iteration. However, this may not be
     as effective due to the nature of linked list traversal being pointer-based.
 */
 
 // TODO: Get Time/Space Complexity Update
+
+use crate::structs::list_node::ListNode;
+
+pub fn add_two_numbers_iteratively_loop_unroll(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+) -> Option<Box<ListNode>> {
+    let mut result_head = Box::new(ListNode::new(0));
+    let mut current = &mut result_head;
+    let mut carry = 0;
+    let mut l1_ = l1;
+    let mut l2_ = l2;
+
+    while l1_.is_some() || l2_.is_some() || carry != 0 {
+        let mut sums = [0, 0, 0, 0];
+        let mut processed = 0;
+
+        // Unroll the original loop by processing the digits in 4-decimal-place increments.
+        for i in 0..4 {
+            if l1_.is_some() || l2_.is_some() || carry != 0 {
+                processed += 1;
+
+                let mut sum = carry;
+
+                if let Some(node) = l1_.take() {
+                    sum += node.val;
+                    l1_ = node.next;
+                }
+
+                if let Some(node) = l2_.take() {
+                    sum += node.val;
+                    l2_ = node.next;
+                }
+
+                carry = sum / 10;
+                sums[i] = sum % 10;
+            }
+        }
+
+        // Apply the calculated nodes
+        for i in 0..processed {
+            let new_node = Box::new(ListNode::new(sums[i]));
+            current.next = Some(new_node);
+            current = current.next.as_mut().unwrap();
+        }
+    }
+
+    result_head.next
+}
--- src/linked_list/medium/mod.rs
+++ src/linked_list/medium/mod.rs
@@ -5,11 +5,13 @@ pub mod add_two_numbers_iteratively_mem_pooling;
 pub mod add_two_numbers_recursively;
 
 #[cfg(test)]
-mod add_two_numbers_iteratively_tests;
+mod add_two_numbers_iteratively_loop_unroll_tests;
 #[cfg(test)]
 mod add_two_numbers_iteratively_mem_aligned_tests;
 #[cfg(test)]
 mod add_two_numbers_iteratively_mem_pooling_tests;
+#[cfg(test)]
+mod add_two_numbers_iteratively_tests;
 #[cfg(test)]
 mod add_two_numbers_recursively_tests;
 #[cfg(test)]
