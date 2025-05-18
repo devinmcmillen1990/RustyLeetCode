b6ecbd224215a251570eab5f6cb793d4ef801832
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-05-18 14:05:01 -0700

    Restructure project to allow for adding multiple problems to the same folder without overwhelming it.

--- src/linked_list/medium/mod.rs
+++ src/linked_list/medium/mod.rs
@@ -1,18 +1,1 @@
-pub mod add_two_numbers_iteratively;
-pub mod add_two_numbers_iteratively_loop_unroll;
-pub mod add_two_numbers_iteratively_mem_aligned;
-pub mod add_two_numbers_iteratively_mem_pooling;
-pub mod add_two_numbers_recursively;
-
-#[cfg(test)]
-mod add_two_numbers_iteratively_loop_unroll_tests;
-#[cfg(test)]
-mod add_two_numbers_iteratively_mem_aligned_tests;
-#[cfg(test)]
-mod add_two_numbers_iteratively_mem_pooling_tests;
-#[cfg(test)]
-mod add_two_numbers_iteratively_tests;
-#[cfg(test)]
-mod add_two_numbers_recursively_tests;
-#[cfg(test)]
-mod add_two_numbers_tests_helpers;
+pub mod add_two_numbers;
