b76c9e45245a338ee3ac25ee40684f1c811561a2
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-05-19 09:51:31 -0700

    Cleanup variable names to make the code more clear.

--- src/arrays/medium/median_of_two_sorted_arrays/median_of_two_sorted_arrays_binary_search.rs
+++ src/arrays/medium/median_of_two_sorted_arrays/median_of_two_sorted_arrays_binary_search.rs
@@ -10,38 +10,38 @@ Time Complexity:    O(log(min(m, n))) — Binary search on the smaller array.
 Space Complexity:   O(1) — Constant space.
 */
 pub fn median_of_two_sorted_arrays_binary_search(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-    let mut a = nums1;
-    let mut b = nums2;
+    let mut nums1_ = nums1;
+    let mut nums2_ = nums2;
 
-    if a.len() > b.len() {
-        swap(&mut a, &mut b);
+    if nums1_.len() > nums2_.len() {
+        swap(&mut nums1_, &mut nums2_);
     }
 
-    let nums1_len = a.len();
-    let nums2_len = b.len();
+    let nums1_len = nums1_.len();
+    let nums2_len = nums2_.len();
     let half_len = (nums1_len + nums2_len + 1) / 2;
-    let mut left = 0;
-    let mut right = nums1_len;
+    let mut main_left = 0;
+    let mut main_right = nums1_len;
 
-    while left <= right {
-        let i = (left + right) / 2;
+    while main_left <= main_right {
+        let i = (main_left + main_right) / 2;
         let j = half_len - i;
 
-        let a_left = if i == 0 { i32::MIN } else { a[i - 1] };
-        let a_right = if i == nums1_len { i32::MAX } else { a[i] };
-        let b_left = if j == 0 { i32::MIN } else { b[j - 1] };
-        let b_right = if j == nums2_len { i32::MAX } else { b[j] };
+        let nums1_left = if i == 0 { i32::MIN } else { nums1_[i - 1] };
+        let nums1_right = if i == nums1_len { i32::MAX } else { nums1_[i] };
+        let nums2_left = if j == 0 { i32::MIN } else { nums2_[j - 1] };
+        let nums2_right = if j == nums2_len { i32::MAX } else { nums2_[j] };
 
-        if a_left <= b_right && b_left <= a_right {
+        if nums1_left <= nums2_right && nums2_left <= nums1_right {
             if (nums1_len + nums2_len) % 2 == 0 {
-                return (a_left.max(b_left) as f64 + a_right.min(b_right) as f64) / 2.0;
+                return (nums1_left.max(nums2_left) as f64 + nums1_right.min(nums2_right) as f64) / 2.0;
             } else {
-                return a_left.max(b_left) as f64;
+                return nums1_left.max(nums2_left) as f64;
             }
-        } else if a_left > b_right {
-            right = i - 1;
+        } else if nums1_left > nums2_right {
+            main_right = i - 1;
         } else {
-            left = i + 1;
+            main_left = i + 1;
         }
     }
 
